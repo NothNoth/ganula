fc7b4445c9c705f2d019783af6a887292146b904
Benoit <benoit@MacBook-Pro-de-Benoit.local>
2023-05-31 15:52:15 +0200

    Getting rid of unused dep

--- tone_generator.cpp
+++ tone_generator.cpp
@@ -1,6 +1,5 @@
 #include "tone_generator.h"
 #include "setup.h"
-#include "interpolate.hpp"
 #ifdef _GRANULA_TESTS_
   #include "granula_tests_stubs.h"
 #else
@@ -121,43 +120,6 @@ unsigned int tone_generate_custom(volatile unsigned short*buffer, unsigned short
   extrapolate(custom_wave, custom_wave_size, buffer, sample_count);
 
   return sample_count;
-}
-using namespace mlinterp;
-
-
-void extrapolate_new(unsigned short *src, int src_size, volatile unsigned short *dest, int dest_size) {
-int input_size = src_size, input_size_array[] = { src_size };
-int output_size = dest_size;
-int input_x[input_size];
-int input_y[input_size];
-int output_x[output_size];
-int output_y[output_size];
-
-for (int i = 0; i < input_size; i++) {
-  input_x[i] = i;
-  input_y[i] = (int)(src[i]);
-}
-for (int i = 0; i < output_size; i++) {
-  output_x[i] = i;
-}
-memset(output_y, 42, output_size);
-interp(
-	input_size_array, output_size, // Number of points
-	input_y, output_y, // Output axis (y)
-	input_x, output_x  // Input axis (x)
-);
-
-for (int i = 0; i < output_size; i++) {
-  dest[i] = (unsigned short) (output_y[i]);
-}
- 
- /* int srcx[src_size];
-  int destx[dest_size];
-  
-  interp(src_size, dest_size,
-        src, dest,
-        srcx, destx);
-*/
 }
 
 void extrapolate(unsigned short *src, int src_size, volatile unsigned short *dest, int dest_size) {
